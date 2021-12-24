bf606003b645c88fb621039505068c46139a66b6
shouravrahman <shouravrahman006@gmail.com>
2021-12-24 19:11:10 +0600

    gitignore,server db connect

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(
+	fileUpload({
+		useTempFiles: true,
+	})
+)
+
+//db connect
+const URI = process.env.MONGO_URI
+
+mongoose.connect(URI, {}, (err) => {
+	if (err) throw err
+
+	console.log('database connected')
+})
+
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => res.send('Hello nigggga'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
