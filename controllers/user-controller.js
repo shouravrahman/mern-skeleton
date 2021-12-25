31d731513884aef2bf9b16a14f449ec499624684
shouravrahman <shouravrahman006@gmail.com>
2021-12-26 01:01:26 +0600

    verification email works

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,17 +1,11 @@
 const User = require('../models/user-model')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const sendMail = require('../utils/sendMail')
+
+const { CLIENT_URL } = process.env
 const userCtrl = {
 	register: async (req, res) => {
-		const validateEmail = (email) => {
-			return String(email)
-				.toLowerCase()
-				.match(
-					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-				)
-		}
-		const validatePassword = (password) => {
-			return String(password).match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/)
-		}
 		try {
 			const { name, email, password } = req.body
 
@@ -35,11 +29,37 @@ const userCtrl = {
 				password: passwordHash,
 			}
 
-			res.json(newUser)
+			const activation_token = createActivationToken(newUser)
+
+			const url = `${CLIENT_URL}/user/activate/${activation_token}`
+			sendMail(email, url, 'Verify your email address')
+
+			res.json({
+				msg: 'Register success! please check your email to activate your account',
+			})
 		} catch (error) {
 			return res.status(500).json({ msg: error.message })
 		}
 	},
+}
+const validateEmail = (email) => {
+	return String(email)
+		.toLowerCase()
+		.match(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		)
+}
+const validatePassword = (password) => {
+	return String(password).match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/)
+}
+const createActivationToken = (payload) => {
+	return jwt.sign(payload, process.env.ACTIVATION_TOKEN_SECRET, { expiresIn: '5m' })
+}
+const createAccessToken = (payload) => {
+	return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20m' })
+}
+const createRefreshToken = (payload) => {
+	return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
 }
 
 module.exports = userCtrl
