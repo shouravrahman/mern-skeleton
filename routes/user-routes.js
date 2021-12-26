73917b77ad9833fc7c1b99f4ae641d1e57c7d61a
shouravrahman <shouravrahman006@gmail.com>
2021-12-26 11:46:47 +0600

    acc tkn route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,5 +4,6 @@ const userCtrl = require('../controllers/user-controller')
 router.post('/register', userCtrl.register)
 router.post('/activation', userCtrl.activateEmail)
 router.post('/login', userCtrl.login)
+router.post('/refresh_token', userCtrl.getAccessToken)
 
 module.exports = router
