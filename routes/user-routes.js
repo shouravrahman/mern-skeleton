25b2eb7c9d634e80cc7cb40107024720b254f4f7
shouravrahman <shouravrahman006@gmail.com>
2021-12-26 12:15:57 +0600

    reset route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const userCtrl = require('../controllers/user-controller')
+const auth = require('../middlewares/auth')
 
 router.post('/register', userCtrl.register)
 router.post('/activation', userCtrl.activateEmail)
 router.post('/login', userCtrl.login)
 router.post('/refresh_token', userCtrl.getAccessToken)
 router.post('/forgotPassword', userCtrl.forgotPassword)
+router.post('/resetPassword', authCheck, userCtrl.resetPassword)
 
 module.exports = router
