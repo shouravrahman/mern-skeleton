299480078af597d726431aa0b1276bbee8f772d5
shouravrahman <shouravrahman006@gmail.com>
2021-12-26 11:47:30 +0600

    forgot pass route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -5,5 +5,6 @@ router.post('/register', userCtrl.register)
 router.post('/activation', userCtrl.activateEmail)
 router.post('/login', userCtrl.login)
 router.post('/refresh_token', userCtrl.getAccessToken)
+router.post('/forgotPassword', userCtrl.forgotPassword)
 
 module.exports = router
