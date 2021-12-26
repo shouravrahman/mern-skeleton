5f413ea3f10c5bf360205fcb34a0cc58f162b311
shouravrahman <shouravrahman006@gmail.com>
2021-12-26 10:59:25 +0600

    activation,login ctrl

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -41,6 +41,52 @@ const userCtrl = {
 			return res.status(500).json({ msg: error.message })
 		}
 	},
+	activateEmail: async (req, res) => {
+		try {
+			const { activation_token } = req.body
+
+			const user = jwt.verify(activation_token, process.env.ACTIVATION_TOKEN_SECRET)
+
+			const { name, email, password } = user
+			const check = await User.findOne({ email })
+			if (check) return res.status(400).json({ msg: 'this email already exists' })
+
+			const newUser = await new User({ name, email, password })
+
+			await newUser.save()
+
+			res.json({ msg: 'Account has been activated' })
+		} catch (error) {
+			return res.status(500).json({ msg: error.message })
+		}
+	},
+	login: async (req, res) => {
+		try {
+			const { email, password } = req.body
+
+			const user = await User.findOne({ email })
+
+			if (!user)
+				return res
+					.status(400)
+					.json({ msg: 'This email does not exist.Please register first' })
+
+			const isMatch = await bcrypt.compare(password, user.password)
+			if (!isMatch) return res.status(400).json({ msg: 'Incorrect password' })
+
+			const refresh_token = createRefreshToken({ id: user._id })
+
+			res.cookie('refreshToken', refresh_token, {
+				httpOnly: true,
+				path: '/user/refresh_token',
+				maxAge: 7 * 24 * 60 * 60 * 1000, //7d
+			})
+
+			return res.json({ msg: 'Login success' })
+		} catch (error) {
+			return res.status(500).json({ msg: error.message })
+		}
+	},
 }
 const validateEmail = (email) => {
 	return String(email)
