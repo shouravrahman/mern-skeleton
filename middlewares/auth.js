3c8294e4ab84ba5f318c2d0cff515890fa616eba
shouravrahman <shouravrahman006@gmail.com>
2021-12-29 16:51:53 +0600

    model import

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
-
-export const authCheck = (req, res, next) => {
+const User = require('../models/user-model')
+const authCheck = async (req, res, next) => {
 	try {
 		const token = req.header('Authorization')
 		if (!token) return res.status(400).json({ msg: 'Invalid authentication.' })
@@ -16,17 +16,18 @@ export const authCheck = (req, res, next) => {
 		return res.status(500).json({ msg: error.message })
 	}
 }
-export const adminCheck = (req, res, next) => {
+const adminCheck = async (req, res, next) => {
 	try {
 		const user = await User.findOne({ _id: req.user.id })
 
 		if (user.role !== 1)
 			return res.status(400).json({ msg: 'Admin resource access denied' })
 
-		// next()
+		next()
 	} catch (error) {
 		return res.status(500).json({ msg: error.message })
 	}
 }
+module.exports = { authCheck, adminCheck }
 
 //TODO: export all functions from this file,require user model,make async ==========> *done*
