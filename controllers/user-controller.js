0258dbd1c4e1e67d1c7dc30cc342fffd1b455a5f
shouravrahman <shouravrahman006@gmail.com>
2021-12-29 17:32:50 +0600

    acc token expire update

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -191,7 +191,7 @@ const createActivationToken = (payload) => {
 	return jwt.sign(payload, process.env.ACTIVATION_TOKEN_SECRET, { expiresIn: '5m' })
 }
 const createAccessToken = (payload) => {
-	return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20m' })
+	return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
 }
 const createRefreshToken = (payload) => {
 	return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
