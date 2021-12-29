f2e3ac89d48c5b0ae057b36390f7b2a853f6b2e0
shouravrahman <shouravrahman006@gmail.com>
2021-12-29 17:35:08 +0600

    fix

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -1,25 +1,33 @@
 const fs = require('fs')
 const removeTmp = require('../utils/removeTmp')
+
 module.exports = async function (req, res, next) {
 	try {
 		if (!req.files || Object.keys(req.files).length === 0)
 			return res.status(400).json({ msg: 'No files were uploaded.' })
+
 		const file = req.files.file
 
 		if (file.size > 1024 * 1024) {
 			removeTmp(file.tempFilePath)
 			return res.status(400).json({ msg: 'Size too large.Maximum file size 1mb' })
-		} //1mb
+		} // 1mb
 
 		if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
 			removeTmp(file.tempFilePath)
 			return res
 				.status(400)
-				.json({ msg: 'File format invalid.Only JPEG and PNG are allowed' })
-		} //format
+				.json({ msg: 'File format is incorrect.Only JPEG and PNG are allowed' })
+		}
 
 		next()
-	} catch (error) {
-		return res.status(500).json({ msg: error.message })
+	} catch (err) {
+		return res.status(500).json({ msg: err.message })
 	}
 }
+
+// const removeTmp = (path) => {
+// 	fs.unlink(path, (err) => {
+// 		if (err) throw err
+// 	})
+// }
